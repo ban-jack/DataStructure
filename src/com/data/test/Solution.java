2c8ea23d68b059b464e142baa160266250deddbc
Break <Break@qq.com>
2021-11-20 14:11:43 +0800

    interview commit -version 1.3.0

--- src/com/data/test/Solution.java
+++ src/com/data/test/Solution.java
@@ -9,64 +9,39 @@ import java.util.regex.Pattern;
 public class Solution {
 
     public static void main(String[] args) {
-        int[] nums = new int[]{-3,-2,-1,0,0,1,2,3};
-        int target = 0;
-        List<List<Integer>> lists = new Solution().fourSum(nums, target);
-        for (List list:lists) {
-            for (Object l:list) {
-                System.out.print(l+" ");
-            }
-            System.out.println();
-        }
+//        Integer a = 1000;
+//        Integer b = 1000;
+//        Integer c = 100;
+//        Integer g = 100;
+//        Integer d = new Integer(100);
+//        Integer e = new Integer(100);
+//        int f = 100;
+//
+//        System.out.println("a==b:"+(a == b));
+//        System.out.println("c==d:"+(c == d));
+//
+//        System.out.println("c==f:"+(c == f));
+//        System.out.println("d==e:"+(d == e));
+//        System.out.println("c==g:"+(c == g));
+        int i = 128;
+        Integer i2 = 128;
+        Integer i3 = new Integer(128);
+        System.out.println("i == i2 = " + (i == i2)); // Integer会自动拆箱为int，所以为true
+        System.out.println("i == i3 = " + (i == i3)); // true，理由同上
+        Integer i4 = 127;// 编译时被翻译成：Integer i4 = Integer.valueOf(127);
+        Integer i5 = 127;
+        System.out.println("i4 == i5 = " + (i4 == i5));// true
+        Integer i6 = 128;
+        Integer i7 = 128;
+        System.out.println("i6 == i7 = " + (i6 == i7));// false
+        Integer i8 = new Integer(127);
+        System.out.println("i5 == i8 = " + (i5 == i8)); // false
+        Integer i9 = new Integer(128);
+        Integer i10 = new Integer(128);
+        System.out.println("i9 == i10 = " + (i9 == i10)); // false
     }
 
-    public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> lists = new ArrayList<>();
-        Arrays.sort(nums);
-        int length = nums.length;
-        for (int i = 0; i < length; i++){
-
-            if(i > 0 && nums[i] == nums[i-1]){
-                continue;
-            }
-
-            for (int j = i + 1; j < length; j++){
-
-                if(j > i + 1 && nums[j] == nums[j-1]){
-                    continue;
-                }
-                int aim = (target - nums[i] - nums[j]);
-                int n = length - 1; // 每当前两个数的和发生改动需要重置 n
-                for (int m = j + 1; m < length; m++){
-
-                    if(m > j + 1 && nums[m] == nums[m-1]){
-                        continue;
-                    }
-
-                    while (n > m && nums[n] + nums[m] > aim){
-                        n--;
-                    }
-
-                    if (n == m){
-                        break;
-                    }
-
-                    if(nums[n] + nums[m] + nums[j] + nums[i] == target){
-
-                        List<Integer> list = new ArrayList<>();
-                        list.add(nums[i]);
-                        list.add(nums[j]);
-                        list.add(nums[m]);
-                        list.add(nums[n]);
-                        lists.add(list);
-
-                    }
-                }
-
-            }
-        }
-        return lists;
-
-    }
 
 }
+
+
