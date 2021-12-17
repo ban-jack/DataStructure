1831d0b965994f71ee57fa7d86341a0fc2da3664
Break <Break@qq.com>
2021-12-17 15:04:42 +0800

    interview commit -version 1.3.1

--- src/com/data/test/Solution.java
+++ src/com/data/test/Solution.java
@@ -7,12 +7,10 @@ import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import com.alibaba.fastjson.JSON;
-
-
-
 public class Solution {
-
+    public static void main(String[] args) {
+        System.out.println("ssss");
+    }
 }
 
 
