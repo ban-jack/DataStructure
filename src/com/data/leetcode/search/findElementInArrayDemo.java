734074468b5f0924465a135572f0eedd8ff5d24c
Break <Break@qq.com>
2022-01-01 20:39:28 +0800

    leetcode commit -version 1.3.5

--- src/com/data/leetcode/search/findElementInArrayDemo.java
+++ src/com/data/leetcode/search/findElementInArrayDemo.java
@@ -1,22 +1,36 @@
-package com.data.test;
-import com.data.leetcode.linkedlist.ListNode;
-import jdk.nashorn.internal.objects.NativeDate;
+package com.data.leetcode.search;
 
-import java.text.ParseException;
-import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+/*
+34. 在排序数组中查找元素的第一个和最后一个位置
+给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+如果数组中不存在目标值 target，返回 [-1, -1]。
 
-public class Solution {
+进阶：
+你可以设计并实现时间复杂度为 O(log n) 的算法解决此问题吗？
 
-    public static void main(String[] args) {
-        int[] nums = new int[]{5,7,7,7,7,10};
-//        int[] nums = new int[]{1,2};
-        int target = 7;
-        int[] ints = new Solution().searchRange1(nums, target);
-        System.out.println(Arrays.toString(ints));
+示例 1：
+输入：nums = [5,7,7,8,8,10], target = 8
+输出：[3,4]
 
-    }
+示例 2：
+输入：nums = [5,7,7,8,8,10], target = 6
+输出：[-1,-1]
+
+示例 3：
+输入：nums = [], target = 0
+输出：[-1,-1]
+
+提示：
+0 <= nums.length <= 105
+-109 <= nums[i] <= 109
+nums 是一个非递减数组
+-109 <= target <= 109
+
+https://leetcode-cn.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+
+ */
+public class findElementInArrayDemo {
+    // 优质解法
     public int[] searchRange1(int[] nums, int target) {
         int[] res = new int[] {-1, -1};
         res[0] = binarySearch(nums, target, true);
@@ -42,8 +56,9 @@ public class Solution {
                     left = mid + 1;
             }
         }
-             return res;
+        return res;
     }
+    // 自己的解法
     public int[] searchRange(int[] nums, int target) {
 
         int length = nums.length;
@@ -79,8 +94,3 @@ public class Solution {
     }
 
 }
-
-
-
-
-
